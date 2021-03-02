0a1fecc0104548c05513f7beb57107f01dd1746f
Alex Hutiev <abhutiev@gmail.com>
2021-03-02 05:07:10 +0300

    deleted 42 headers

--- srcs/utils/substr.c
+++ srcs/utils/substr.c
@@ -1,20 +1,5 @@
 #include "../../includes/minishell.h"
 
-/*
-**	How the function ft_substr.c works:
-**	1)	Check if argument s is valid (!= NULL).
-**	2)	Try to allocate memory for resulting string.
-**		2.1)	If memory was not allocated, return NULL-pointer.
-**	3)	Check, if index of first element to copy is more than a length of
-**															string we handle.
-**		3.1)	If it's more - return pointer on allocated memory.
-**	4)	Go through the both lines in increments of one element and copy
-**		elements from number "start" in string passed as an argument
-**		to substring.
-**	5)	NULL-terminating of our substring.
-**	6)	Return pointer on first character of built string.
-*/
-
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*substring;
